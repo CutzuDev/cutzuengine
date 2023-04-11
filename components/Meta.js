5a1593f0942b99ef2a9a341f68dd77900f98f845
CutzuDev <alexfarkas05@gmail.com>
2023-04-12 01:02:26 +0300

    Trying out meta tags for next

---
+++ components/Meta.js
@@ -0,0 +1,48 @@
+import Head from "next/head";
+
+function Meta() {
+  return (
+    <Head>
+      <meta charset="UTF-8" />
+      <meta name="theme-color" content="#ffa600" />
+      <meta name="title" content="cutzuENGINE" />
+      <meta
+        name="description"
+        content="cutzuENGINE got released!!! (joke project)"
+      />
+
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="English" />
+      <meta name="author" content="CutzuSD" />
+
+      <title>cutzuENGINE</title>
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://cutzuengine.vercel.app/" />
+      <meta property="og:title" content="cutzuENGINE" />
+      <meta
+        property="og:description"
+        content="cutzuENGINE got released!!! (joke project)"
+      />
+      <meta
+        property="og:image"
+        content="https://media.discordapp.net/attachments/1090015180496322561/1095468655380275200/redlogo.png"
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://cutzuengine.vercel.app/" />
+      <meta property="twitter:title" content="cutzuENGINE" />
+      <meta
+        property="twitter:description"
+        content="cutzuENGINE got released!!! (joke project)"
+      />
+      <meta
+        property="twitter:image"
+        content="https://media.discordapp.net/attachments/1090015180496322561/1095468655380275200/redlogo.png"
+      />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+  );
+}
+
+export default Meta;
